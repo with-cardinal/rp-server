a673907b922784bc3aec48c6b9bad3b05a226b14
Alan Johnson <nalanj@fastmail.com>
2023-01-05 14:28:02 -0500

    Pull in types from client

--- src/types.ts
+++ src/types.ts
@@ -1,20 +1,28 @@
-import type { ValidJSON } from "@withcardinal/ts-std";
-
-export type RPSpec = {
-  versions: Record<string, VersionSpec>;
-};
-
-export type VersionSpec = Record<string, ProcedureSpec>;
+import type { ValidJSON, ValidJSONObject } from "@withcardinal/ts-std";
 
 export type Authorization = {
   scheme: string;
   token: string;
 };
 
-export type ProcedureSpec = {
-  mutation?: boolean;
-  proc: (
-    auth: Authorization,
-    payload: unknown
-  ) => ValidJSON | Promise<ValidJSON>;
+export type RPSpec = {
+  versions: Record<string, VersionSpec>;
 };
+
+// eslint-disable-next-line @typescript-eslint/no-explicit-any
+export type VersionSpec = Record<string, ProcedureSpec<any, any>>;
+
+export type ProcedureReturn = ValidJSON | Promise<ValidJSON>;
+
+export type ProcedureSpec<
+  T extends ValidJSONObject,
+  R extends ProcedureReturn
+> = {
+  mutation?: boolean;
+  proc: Procedure<T, R>;
+};
+
+type Procedure<T extends ValidJSONObject, R extends ProcedureReturn> = (
+  auth: Authorization,
+  payload: T
+) => R;
